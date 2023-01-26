90b5c2bd608c0d4048027b597728a85946e504dc
Magma <raymondamoateng@gmail.com>
2023-01-26 16:49:55 +0000

    Whyyyyy!

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -16,10 +16,9 @@ def pascal_triangle(n):
         return []
 
     for i in range(n):
-        for p in range(i + 1):
-            if p == 0 or p == i:
+        for j in range(i + 1):
+            if j == 0 or j == i:
                 arr[i].append(1)
             else:
-                arr[i].append(arr[i - 1][p - 1] + arr[i - 1][p])
+                arr[i].append(arr[i - 1][j - 1] + arr[i - 1][j])
     return arr
-    
