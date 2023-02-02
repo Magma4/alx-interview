8e7911e5aa4f41aa72442f4d29a4ed2ecbd0fbbf
Magma <raymondamoateng@gmail.com>
2023-02-02 16:23:04 +0000

    Debugging

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
-    keys = [0]
-    for key in keys:
-        for boxKey in boxes[key]:
-            if boxKey not in keys and boxKey < len(boxes):
-                keys.append(boxKey)
-    if len(keys) == len(boxes):
-        return True
+    unlocked = set()
+
+    for box_id, box in enumerate(boxes):
+        if len(box) == 0 or box_id == 0:
+            unlocked.add(box_id)
+        for key in box:
+            if key < len(boxes) and key != box_id:
+                unlocked.add(key)
+        if len(unlocked) == len(boxes):
+            return True
     return False
