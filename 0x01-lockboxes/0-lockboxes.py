d28a951c5569798e5d64e0aa82fc564adaea4108
Magma <raymondamoateng@gmail.com>
2023-02-02 16:16:40 +0000

    Script

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
