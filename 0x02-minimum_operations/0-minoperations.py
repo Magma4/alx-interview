386ff327b0a931eb08a353828576c44566871058
Magma <raymondamoateng@gmail.com>
2023-02-09 10:10:06 +0000

    Debugging

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 
 """
-A method that calculates the fewest number of operations needed to result in exactly n H characters in the file
+    Method that determines the number of minmum operations given n characters
 """
 
 
 def minOperations(n):
     """
-    Prototype: def minOperations(n)
-    Returns an integer If n is impossible to achieve, return 0
+        A function that calculates the fewest number of operations
+        needed to give a result of exactly n H characters in a file
+        args: n: Number of characters to be displayed
+        return:
+               number of min operations
     """
+
     now = 1
     start = 0
     counter = 0
