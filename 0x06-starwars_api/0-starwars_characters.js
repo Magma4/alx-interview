8f9f20170c0bf17369cbde15db8b80ac76c2e6fc
Magma <raymondamoateng@gmail.com>
2023-03-05 13:47:37 +0000

    May the force be with me. Lol

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,23 @@
 #!/usr/bin/node
+
 const request = require('request');
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
-  if (!err) {
-    const characters = JSON.parse(body).characters;
-    recursionRequestPrint(characters, 0);
+
+const movieId = process.argv[2];
+
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, async (err, res, body) => {
+  err && console.log(err);
+
+  const charactersArray = (JSON.parse(res.body).characters);
+  for (const character of charactersArray) {
+    await new Promise((resolve, reject) => {
+      request(character, (err, res, body) => {
+        err && console.log(err);
+
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
   }
 });
-
-function recursionRequestPrint (url, index) {
-  request(url[index], (err, res, body) => {
-    if (!err) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < url.length) recursionRequestPrint(url, ++index);
-    }
-  });
-}
